81e64ea249dd938161262ba851f9559592f77ef2
zedwar <1245918327@qq.com>
2021-06-16 09:44:26 +0800

    完成ajax

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 let n = 1;
 getPage.onclick = () => {
   const request = new XMLHttpRequest();
-  request.open("GET", `/page${n+1}`);
+  request.open("GET", `/page${n + 1}`);
   request.onreadystatechange = () => {
     if (request.readyState === 4 && request.status === 200) {
       const array = JSON.parse(request.response);
@@ -10,7 +10,7 @@ getPage.onclick = () => {
         li.textContent = item.id;
         xxx.appendChild(li);
       });
-      n+=1
+      n += 1
     }
   };
   request.send();
@@ -43,30 +43,30 @@ getXML.onclick = () => {
 };
 getHTML.onclick = () => {
   const request = new XMLHttpRequest();
-  request.open("GET", "/3.htm");
+  request.open("GET", "/3.html");
   request.onload = () => {
-    // 创建 div 标签
+
     const div = document.createElement("div");
-    // 填写 div 内容
+
     div.innerHTML = request.response;
-    // 插入到身体里
+
     document.body.appendChild(div);
   };
-  request.onerror = () => {};
+  request.onerror = () => { };
   request.send();
 };
 getJS.onclick = () => {
   const request = new XMLHttpRequest();
   request.open("GET", "/2.js");
   request.onload = () => {
-    // 创建 script 标签
+
     const script = document.createElement("script");
-    // 填写 script 内容
+
     script.innerHTML = request.response;
-    // 插到身体里
+
     document.body.appendChild(script);
   };
-  request.onerror = () => {};
+  request.onerror = () => { };
   request.send();
 };
 
@@ -75,19 +75,19 @@ getCSS.onclick = () => {
   request.open("GET", "/style.css"); // readyState = 1
   request.onreadystatechange = () => {
     console.log(request.readyState);
-    // 下载完成，但不知道是成功 2xx 还是失败 4xx 5xx
+
     if (request.readyState === 4) {
       if (request.status >= 200 && request.status < 300) {
-        // 创建 style 标签
+
         const style = document.createElement("style");
-        // 填写 style 内容
+
         style.innerHTML = request.response;
-        // 插到头里面
+
         document.head.appendChild(style);
       } else {
         alert("加载 CSS 失败");
       }
     }
   };
-  request.send(); // readyState = 2
+  request.send(); 
 };
